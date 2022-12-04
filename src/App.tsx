d3c414c583363d1f1454d57d77ee1323e4213b3f
KauanPecanha <kauanpecanha1@gmail.com>
2022-12-04 09:55:19 -0300

    Teste de Styled Components na estilização de um parágrafo.

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import logo from './logo.svg';
 
 import { Title } from "./styles"
 import { TitleSmall } from "./styles"
+import { Paragraph } from "./styles"
 
 function App() {
   return (
@@ -14,6 +15,11 @@ function App() {
       </Title>
 
       <TitleSmall>Menor</TitleSmall>
+      <div className="content">
+        <Paragraph>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat eveniet veniam quis id sapiente, quod voluptate cupiditate laudantium, voluptatem minima cumque cum ullam ducimus facere distinctio voluptas, reiciendis voluptatibus vero dolorum numquam deserunt illum placeat excepturi! Nostrum praesentium cupiditate pariatur ex delectus et qui doloremque animi neque necessitatibus ut dicta totam sequi, aut consequatur vel fugiat consequuntur ullam nulla laborum adipisci nemo vero minima reiciendis. Inventore quae consequuntur nihil explicabo non consequatur vero ipsum sunt culpa ipsam. Dolor, quis consequatur?
+        </Paragraph>
+      </div>
     </div>
   );
 }
--- src/styles.js
+++ src/styles.js
@@ -1,17 +1,31 @@
 import styled from 'styled-components'
 
 export const Title = styled.h1`
+    // como deseja-se que Title tenha as características de um h1, então chama-se a instância do método styled.h1
     color: #f00;
 
     font-size: ${props => `${props.fontSize}px`};
     background-color: black;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
 
     span{
+        // como span está dentro de title, pode-se atribuir características a ele, o chamando dentro das strings literais(´´).
         font-size: 12px;
     }
 `;
 
-export const TitleSmall = styled(Title)`
+export const TitleSmall = styled(Title)` 
+    // TitleSmall herda todas as características de Title
     color: #00f;
     font-size: 16px;
-`
+`;
+
+export const Paragraph = styled.p`
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+`;
